5167bef6ee370acb5b89c2f914b5da0fe1969d37
HeavenOSK <heavenosk@gmail.com>
2021-03-06 23:11:34 +0900

    modify: card ui

--- app_store_route/lib/app_store_card.dart
+++ app_store_route/lib/app_store_card.dart
@@ -1,74 +1,150 @@
 import 'package:flutter/material.dart';
 
-class AppStoreCard extends StatelessWidget {
+class AppStoreCard extends StatefulWidget {
   const AppStoreCard({Key? key}) : super(key: key);
+
+  static const size = Size(360, 434);
+
+  @override
+  _AppStoreCardState createState() => _AppStoreCardState();
+}
+
+class _AppStoreCardState extends State<AppStoreCard> {
+  bool _pressing = false;
+
+  Size get preferredSize =>
+      !_pressing ? AppStoreCard.size : AppStoreCard.size * 0.97;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
-    return Column(
-      children: [
-        AspectRatio(
-          aspectRatio: 400 / 300,
-          child: ClipRect(
-            child: Image.asset(
-              'images/image_3.jpg',
-              fit: BoxFit.fitWidth,
-            ),
-          ),
+    return GestureDetector(
+      onTapDown: (_) {
+        setState(() {
+          _pressing = true;
+        });
+      },
+      onTapUp: (_) {
+        setState(() {
+          _pressing = false;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          _pressing = false;
+        });
+      },
+      child: TweenAnimationBuilder<Size>(
+        tween: Tween<Size>(
+          begin: AppStoreCard.size,
+          end: preferredSize,
         ),
-        Container(
-          color: Colors.white,
-          height: 80,
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: [
-                AspectRatio(
-                  aspectRatio: 1,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.asset(
-                      'images/image_3.jpg',
-                      fit: BoxFit.fitWidth,
+        curve: Curves.easeOut,
+        duration: Duration(milliseconds: 260),
+        builder: (_, size, __) {
+          return SizedBox.fromSize(
+            size: AppStoreCard.size,
+            child: Center(
+              child: SizedBox.fromSize(
+                size: size,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: SizedBox.fromSize(
+                    size: AppStoreCard.size,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 20,
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            AspectRatio(
+                              aspectRatio: 400 / 360,
+                              child: ClipRect(
+                                child: Image.asset(
+                                  'images/image_3.jpg',
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: Row(
+                                    children: [
+                                      AspectRatio(
+                                        aspectRatio: 1,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          child: Image.asset(
+                                            'images/image_3.jpg',
+                                            fit: BoxFit.fitWidth,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 16),
+                                      FittedBox(
+                                        fit: BoxFit.fitHeight,
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'Developer',
+                                              style: theme.textTheme.subtitle2!
+                                                  .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black
+                                                    .withOpacity(0.5),
+                                              ),
+                                            ),
+                                            Text(
+                                              'App',
+                                              style: theme.textTheme.subtitle2!
+                                                  .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            Text(
+                                              'Describe about App',
+                                              style: theme.textTheme.subtitle2!
+                                                  .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black
+                                                    .withOpacity(0.35),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
-                SizedBox(width: 16),
-                FittedBox(
-                  fit: BoxFit.fitHeight,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Developer',
-                        style: theme.textTheme.subtitle2!.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                      ),
-                      Text(
-                        'App',
-                        style: theme.textTheme.subtitle2!.copyWith(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Describe about App',
-                        style: theme.textTheme.subtitle2!.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black.withOpacity(0.35),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ],
+          );
+        },
+      ),
     );
   }
 }
--- app_store_route/lib/home.dart
+++ app_store_route/lib/home.dart
@@ -11,30 +11,32 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.9,
-            child: Container(
-              color: Colors.greenAccent,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '3月6日 土曜日',
-                    style: theme.textTheme.subtitle2!.copyWith(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black.withOpacity(0.35),
+          child: ListView(
+            children: [
+              FractionallySizedBox(
+                widthFactor: 0.85,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '3月6日 土曜日',
+                      style: theme.textTheme.subtitle2!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black.withOpacity(0.35),
+                      ),
                     ),
-                  ),
-                  Text(
-                    'Today',
-                    style: theme.textTheme.headline4!.copyWith(
-                      fontWeight: FontWeight.bold,
+                    Text(
+                      'Today',
+                      style: theme.textTheme.headline4!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  AppStoreCard(),
-                ],
+                  ],
+                ),
               ),
-            ),
+              SizedBox(height: 12),
+              AppStoreCard(),
+            ],
           ),
         ),
       ),
