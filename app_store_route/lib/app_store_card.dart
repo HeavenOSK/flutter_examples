9d5a7b412def1698b10378c80bdbbedd0a3ab668
HeavenOSK <heavenosk@gmail.com>
2021-03-07 00:45:02 +0900

    add: appContainerRoute

--- app_store_route/lib/app_store_card.dart
+++ app_store_route/lib/app_store_card.dart
@@ -1,3 +1,4 @@
+import 'package:app_store_route/static_card.dart';
 import 'package:flutter/material.dart';
 
 class AppStoreCard extends StatefulWidget {
@@ -14,11 +15,21 @@ class _AppStoreCardState extends State<AppStoreCard> {
 
   Size get preferredSize =>
       !_pressing ? AppStoreCard.size : AppStoreCard.size * 0.97;
+  final _containerKey = GlobalKey();
+  final _areaKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return GestureDetector(
+      key: _areaKey,
+      onTap: () {
+        Navigator.of(context).push(
+          _AppOpenContainerRoute(
+            containerKey: _containerKey,
+            areaKey: _areaKey,
+          ),
+        );
+      },
       onTapDown: (_) {
         setState(() {
           _pressing = true;
@@ -46,101 +57,184 @@ class _AppStoreCardState extends State<AppStoreCard> {
             size: AppStoreCard.size,
             child: Center(
               child: SizedBox.fromSize(
+                key: _containerKey,
                 size: size,
                 child: FittedBox(
                   fit: BoxFit.fitWidth,
-                  child: SizedBox.fromSize(
-                    size: AppStoreCard.size,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            blurRadius: 20,
-                            spreadRadius: 0,
+                  child: StaticCard(
+                    radius: 16,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _AppOpenContainerRoute<T> extends ModalRoute<T> {
+  _AppOpenContainerRoute({
+    required this.containerKey,
+    required this.areaKey,
+  });
+
+  final GlobalKey containerKey;
+  final GlobalKey areaKey;
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color? get barrierColor => null;
+
+  @override
+  bool get opaque => true;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  String? get barrierLabel => null;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  Duration get reverseTransitionDuration => const Duration(milliseconds: 250);
+
+  RenderBox? _navigator(BuildContext context) {
+    return Navigator.of(context).context.findRenderObject() as RenderBox?;
+  }
+
+  Rect _rectForKey(GlobalKey key, RenderBox rootRenderBox) {
+    final render = key.currentContext!.findRenderObject() as RenderBox?;
+    return MatrixUtils.transformRect(
+      render!.getTransformTo(rootRenderBox),
+      Offset.zero & render.size,
+    );
+  }
+
+  @override
+  Widget buildPage(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+  ) {
+    final navigator = _navigator(context);
+    final containerRect = _rectForKey(
+      containerKey,
+      navigator!,
+    );
+    print(containerRect.topLeft);
+    final areaRect = _rectForKey(
+      areaKey,
+      navigator,
+    );
+    print(areaRect.topLeft);
+    final pageRect = Offset.zero & navigator.size;
+    print(pageRect.topLeft);
+
+    return Align(
+      alignment: Alignment.topLeft,
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, _) {
+          Curve _preferredCurve() {
+            switch (animation.status) {
+              case AnimationStatus.dismissed:
+              case AnimationStatus.completed:
+              case AnimationStatus.forward:
+                return Curves.easeOut;
+              case AnimationStatus.reverse:
+                return Curves.easeIn;
+            }
+          }
+
+          Tween<Offset> _preferredOffsetTween() {
+            switch (animation.status) {
+              case AnimationStatus.dismissed:
+              case AnimationStatus.completed:
+              case AnimationStatus.forward:
+                return Tween<Offset>(
+                  begin: containerRect.topLeft,
+                  end: pageRect.topLeft,
+                );
+              case AnimationStatus.reverse:
+                return Tween<Offset>(
+                  begin: areaRect.topLeft,
+                  end: pageRect.topLeft,
+                );
+            }
+          }
+
+          Tween<Size> _preferredSizeTween() {
+            switch (animation.status) {
+              case AnimationStatus.dismissed:
+              case AnimationStatus.completed:
+              case AnimationStatus.forward:
+                return Tween<Size>(
+                  begin: containerRect.size,
+                  end: pageRect.size,
+                );
+              case AnimationStatus.reverse:
+                return Tween<Size>(
+                  begin: areaRect.size,
+                  end: pageRect.size,
+                );
+            }
+          }
+
+          final curve = CurvedAnimation(
+            parent: animation,
+            curve: _preferredCurve(),
+          );
+          final offset = _preferredOffsetTween().transform(curve.value);
+          final size = _preferredSizeTween().transform(curve.value);
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: Stack(
+              children: [
+                Positioned(
+                  top: offset.dy,
+                  left: offset.dx,
+                  child: StaticCard(
+                    radius: 16 - curve.value * 16,
+                    isPage: true,
+                    size: size,
+                  ),
+                ),
+                SafeArea(
+                  child: Opacity(
+                    opacity: 1,
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.white.withOpacity(0.9),
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Icon(
+                              Icons.close,
+                              color: Colors.grey,
+                              size: 20,
+                            ),
                           ),
-                        ],
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(16),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            AspectRatio(
-                              aspectRatio: 400 / 360,
-                              child: ClipRect(
-                                child: Image.asset(
-                                  'images/image_3.jpg',
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                color: Colors.white,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16),
-                                  child: Row(
-                                    children: [
-                                      AspectRatio(
-                                        aspectRatio: 1,
-                                        child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                          child: Image.asset(
-                                            'images/image_3.jpg',
-                                            fit: BoxFit.fitWidth,
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(width: 16),
-                                      FittedBox(
-                                        fit: BoxFit.fitHeight,
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Developer',
-                                              style: theme.textTheme.subtitle2!
-                                                  .copyWith(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black
-                                                    .withOpacity(0.5),
-                                              ),
-                                            ),
-                                            Text(
-                                              'App',
-                                              style: theme.textTheme.subtitle2!
-                                                  .copyWith(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            Text(
-                                              'Describe about App',
-                                              style: theme.textTheme.subtitle2!
-                                                  .copyWith(
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.black
-                                                    .withOpacity(0.35),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
           );
         },
---
+++ app_store_route/lib/static_card.dart
@@ -0,0 +1,149 @@
+import 'package:app_store_route/app_store_card.dart';
+import 'package:flutter/material.dart';
+
+class StaticCard extends StatelessWidget {
+  const StaticCard({
+    required this.radius,
+    this.isPage = false,
+    this.size,
+    Key? key,
+  }) : super(key: key);
+
+  final double radius;
+  final bool isPage;
+  final Size? size;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    if (!isPage) {
+      return _headingContainer(theme);
+    }
+    final _size = size;
+    if (_size == null) {
+      return SizedBox.shrink();
+    }
+
+    final ratio = _size.width / AppStoreCard.size.width;
+    final preferredCardSize = AppStoreCard.size * ratio;
+    return SizedBox.fromSize(
+      size: _size,
+      child: ClipRect(
+        child: ListView(
+          children: [
+            SizedBox.fromSize(
+              size: preferredCardSize,
+              child: FittedBox(
+                child: _headingContainer(theme),
+              ),
+            ),
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Text(
+                'なんかめちゃくちゃむずい',
+                style: theme.textTheme.bodyText2!.copyWith(
+                  color: Colors.black.withOpacity(0.5),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox _headingContainer(ThemeData theme) {
+    return SizedBox.fromSize(
+      size: AppStoreCard.size,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(radius),
+          border: Border.all(
+            width: 0,
+            color: Colors.black.withOpacity(isPage ? 1 : 0),
+          ),
+          boxShadow: [
+            if (!isPage)
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                blurRadius: 20,
+                spreadRadius: 0,
+              ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(radius),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AspectRatio(
+                aspectRatio: 400 / 360,
+                child: ClipRect(
+                  child: Image.asset(
+                    'images/image_3.jpg',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 1,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              'images/image_3.jpg',
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        FittedBox(
+                          fit: BoxFit.fitHeight,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Developer',
+                                style: theme.textTheme.subtitle2!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black.withOpacity(0.5),
+                                ),
+                              ),
+                              Text(
+                                'App',
+                                style: theme.textTheme.subtitle2!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Describe about App',
+                                style: theme.textTheme.subtitle2!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black.withOpacity(0.35),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
